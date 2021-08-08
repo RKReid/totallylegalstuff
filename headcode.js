d230c423e2d202f87241c2b481290abd620b6a16
RKReid <robkreid05@gmail.com>
2021-08-08 17:58:43 -0400

    legal stuff

---
+++ headcode.js
@@ -0,0 +1,7 @@
+var head="<head>\
+<link rel='icon' href='images/tbwe6 logo.png' type='image/png'/>\
+<link rel='stylesheet' href='main.css'/>\
+<title>TBWE6</title>\
+</head>\
+";
+document.write(head);
